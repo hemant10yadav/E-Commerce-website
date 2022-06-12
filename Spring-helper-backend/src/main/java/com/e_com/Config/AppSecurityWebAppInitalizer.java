6f77a747775816f44e4959203038b613d4ec3590
Hemant Singh Yadav <hemant.10.yadav@gmail.com>
2022-06-12 15:46:08 +0530

    added frontend

---
+++ Spring-helper-backend/src/main/java/com/e_com/Config/AppSecurityWebAppInitalizer.java
@@ -0,0 +1,7 @@
+package com.e_com.Config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class AppSecurityWebAppInitalizer extends AbstractSecurityWebApplicationInitializer {
+
+}
